577530b290790cff5f2bd0d56d1beb475c9d58b3
Dmitry Scherbakov <3ddesign@bk.ru>
2017-02-01 19:51:35 +0200

    react electron 1 part

---
+++ app/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Search from './components/search.component';
+import Details from './components/details.component';
+import Player from './components/player.component';
+import Progress from './components/progress.component';
+import Footer from './components/footer.component';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Search/>
+                <Details title="Track title"/>
+                <Player/>
+                <Progress
+                    position={"0.1"}
+                    elapsed={"00:00"}
+                    total={"00:50"}
+                />
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('content'));
---
+++ app/components/footer.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer  extends React.Component {
+    render() {
+        return (
+            <div className="footer">
+                <p>Copied by me &copy; ))</p>
+            </div>
+        );
+    }
+}
+
+export default Footer ;
